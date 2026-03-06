6a9fb3e5256c47ba01c7c47b4776538efc09776d
mscherer <dereuromark@web.de>
2026-03-06 14:31:14 +0100

    Add tests for Body, Query, and Request DTO sources

--- tests/test_app/TestApp/Controller/DependenciesController.php
+++ tests/test_app/TestApp/Controller/DependenciesController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace TestApp\Controller;
 
 use Cake\Controller\Attribute\MapRequestToDto;
+use Cake\Controller\Attribute\RequestToDtoSource;
 use Cake\Controller\Controller;
 use Cake\Event\EventManagerInterface;
 use Cake\Http\ServerRequest;
@@ -96,6 +97,27 @@ class DependenciesController extends Controller
 
     public function requestDto(
         #[MapRequestToDto]
+        RequestDataDto $dto,
+    ) {
+        return $this->response->withStringBody(json_encode($dto->toArray()));
+    }
+
+    public function requestDtoBody(
+        #[MapRequestToDto(source: RequestToDtoSource::Body)]
+        RequestDataDto $dto,
+    ) {
+        return $this->response->withStringBody(json_encode($dto->toArray()));
+    }
+
+    public function requestDtoQuery(
+        #[MapRequestToDto(source: RequestToDtoSource::Query)]
+        RequestDataDto $dto,
+    ) {
+        return $this->response->withStringBody(json_encode($dto->toArray()));
+    }
+
+    public function requestDtoRequest(
+        #[MapRequestToDto(source: RequestToDtoSource::Request)]
         RequestDataDto $dto,
     ) {
         return $this->response->withStringBody(json_encode($dto->toArray()));
